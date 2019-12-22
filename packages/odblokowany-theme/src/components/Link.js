5052c9d5c64403d8cf809f0a2b4f49989e73be8b
DamianComarch <damian.trzcinski@comarch.pl>
2019-12-22 14:06:40 +0100

    Links pre fetch on mouse enter

--- packages/odblokowany-theme/src/components/Link.js
+++ packages/odblokowany-theme/src/components/Link.js
@@ -1,16 +1,36 @@
 import React from "react";
 import { connect } from "frontity";
 
-const Link = ({ actions, link, className, children }) => {
+const Link = ({
+  actions,
+  link,
+  className,
+  children,
+  rel,
+  "aria-current": ariaCurrent,
+  onClick: onClickProp
+}) => {
+  const isExternal = link.startsWith("http");
+
   const onClick = event => {
-    if (link.startsWith("http")) return;
+    if (isExternal) return;
     event.preventDefault();
     actions.router.set(link);
     window.scrollTo(0, 0);
+    if (onClickProp) {
+      onClickProp(event);
+    }
   };
 
   return (
-    <a href={link} onClick={onClick} className={className}>
+    <a
+      href={link}
+      onClick={onClick}
+      className={className}
+      aria-current={ariaCurrent}
+      rel={isExternal ? "noopener noreferrer" : rel}
+      onMouseEnter={() => actions.source.fetch(link)}
+    >
       {children}
     </a>
   );
